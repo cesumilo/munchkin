c6c019f61ca8db39a982de096eabf0b635b02265
Guillaume Robin <guillaume.robin@appi-conseil.com>
2019-10-15 23:53:11 +0200

    Extract primary colors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/**
+ * #92784d Muesli (Light Brown)
+ * #5c4632 Deep Bronze (Brown)
+ * #2e2824 Bokara Grey (Black)
+ * #9e9e9c Mountain Mist (Grey)
+ * #9d4a4e Roof Terracotta (Red)
+ */
+
 const HelloWorld = () => (
   <div>
     <h1>Hello World!</h1>
