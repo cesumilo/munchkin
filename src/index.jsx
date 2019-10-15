761d9380511fb534aaa7998956e9efdf0922b748
Guillaume Robin <guillaume.robin@appi-conseil.com>
2019-10-15 23:19:50 +0200

    Configure react project

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+
+// eslint-disable-next-line
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
